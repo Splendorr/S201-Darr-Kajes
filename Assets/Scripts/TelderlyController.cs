863a4cce2ef60d1859d24b34881c860fb4526e96
Tony Ransom <anthonykennethransom@gmail.com>
2014-03-01 12:15:19 -0500

    slt

--- Assets/Scripts/TelderlyController.cs
+++ Assets/Scripts/TelderlyController.cs
@@ -6,6 +6,10 @@ public class TelderlyController : MonoBehaviour
 
 	public GameObject walker;
 
+	//INTS
+
+	public int playerSpeed = 10;
+
 	//BOOLS
 
 	//Which foot needs to be moved in order to advance.  Should pressing the same button twice cause a trip?
@@ -14,11 +18,18 @@ public class TelderlyController : MonoBehaviour
 	void FixedUpdate()
 	{
 		if (Input.GetKeyDown (KeyCode.X) &&
-			rightFootReady) {
+			rightFootReady)
+		{
+			rightFootReady = false;
 			Debug.Log ("Right foot!");
-		} else if (Input.GetKeyDown (KeyCode.Z) &&
+			rigidbody.AddForce(Vector3.forward * playerSpeed);
+		}
+		else if (Input.GetKeyDown (KeyCode.Z) &&
 			!rightFootReady)
 		{
+			rightFootReady = true;
+			Debug.Log ("Left foot!");
+			rigidbody.AddForce(Vector3.forward * playerSpeed);
 		}
 	}
 }
