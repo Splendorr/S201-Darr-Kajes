fd9da58bd5646946a9251293d5fd5851729def6a
Brian Ruggieri <brianruggieri@gmail.com>
2014-03-01 12:15:13 -0500

    updated elder prefab. added walker controller script

--- Assets/Scripts/ElderlyController.cs
+++ Assets/Scripts/ElderlyController.cs
@@ -6,8 +6,16 @@ public class ElderlyController : MonoBehaviour
 
 	public GameObject walker;
 
+	void Start()
+	{
+		rigidbody.centerOfMass = new Vector3(0,0,.1f);
+	}
+
 	void Update()
 	{
-
+//		if(Input.GetKeyDown(KeyCode.Z))
+//		{
+//			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+.5f* Time.deltaTime);
+//		}
 	}
 }
---
+++ Assets/Scripts/WalkerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkerController : MonoBehaviour 
+{
+	public bool isOnGround;
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.LeftShift))
+		{
+			rigidbody.AddRelativeForce(new Vector3(0, -Physics.gravity.y * rigidbody.mass, rigidbody.mass * 5 * Time.deltaTime));
+		}
+	}
+}
