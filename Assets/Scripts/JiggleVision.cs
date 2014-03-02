df297962a7210cf2ca17586f37acf105425796ba
Brian Ruggieri <brianruggieri@gmail.com>
2014-03-01 22:26:04 -0500

    updated jiggle

--- Assets/Scripts/JiggleVision.cs
+++ Assets/Scripts/JiggleVision.cs
@@ -15,30 +15,38 @@ public class JiggleVision : MonoBehaviour {
 	public float zVelocity = 0.0F;
 
 	public GameObject walker;
+	public WalkerController walkerController;
 	
 	// Use this for initialization
 	void Start () {
 //		xPosition = 0;//transform.position.x;
 //		zPosition = 0;//transform.position.z;
 //		walker = gameObject;
+		walkerController = walker.GetComponent<WalkerController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKey(KeyCode.LeftShift)) {
+		if(!walkerController.isOnGround) {
 			getJiggle();
 		}
-		float newRotationX = Mathf.SmoothDamp(transform.position.x, xRotation, ref xVelocity, smoothTime);
-		float newRotationZ = Mathf.SmoothDamp(transform.position.z, zRotation, ref zVelocity, smoothTime);
-		transform.eulerAngles = new Vector3(newRotationX, yRotation, newRotationZ);
+		else
+		{
+			xRotation = 0f;
+			zRotation = 0f;
+			smoothTime = 0f;
+		}
+		float newRotationX = Mathf.SmoothDamp(transform.eulerAngles.x, transform.eulerAngles.x+xRotation, ref xVelocity, smoothTime);
+		float newRotationZ = Mathf.SmoothDamp(transform.eulerAngles.z, transform.eulerAngles.z+zRotation, ref zVelocity, smoothTime);
+		transform.eulerAngles = new Vector3(newRotationX, walker.transform.eulerAngles.y, newRotationZ);
 
 	}
 
 	void getJiggle() {
 
-		xRotation = Random.Range(-2.0f, 2.0f);
-		zRotation = Random.Range(-2.0f, 2.0f);
+		xRotation = Random.Range(-.3f, .3f);
+		zRotation = Random.Range(-.3f, .3f);
 
 		if(walker.transform.position.y < -0.6f) {
 			// state 1
