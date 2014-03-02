6b7307bfb79937cf1f81f888c81a3a4041df0048
Tony Ransom <anthonykennethransom@gmail.com>
2014-03-02 00:36:11 -0500

    Sparklights

---
+++ Assets/Scripts/SparkScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparkScript : MonoBehaviour
+{
+	private float lifeSpan;
+
+	void Start()
+	{
+		lifeSpan = Random.Range (0.01f, 0.1f);
+		StartCoroutine (Die ());
+	}
+
+	IEnumerator Die()
+	{
+		yield return new WaitForSeconds (lifeSpan);
+		Destroy (gameObject);
+	}
+}
