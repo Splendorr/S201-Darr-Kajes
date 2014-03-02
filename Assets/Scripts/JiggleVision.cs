fbbcbee336be395155ef4f57e936ac2c1aa25ed1
Brian Ruggieri <brianruggieri@gmail.com>
2014-03-01 23:43:34 -0500

    camera jiggle bounds, cleanup

--- Assets/Scripts/JiggleVision.cs
+++ Assets/Scripts/JiggleVision.cs
@@ -5,10 +5,6 @@ public class JiggleVision : MonoBehaviour {
 
 	public float xRotation = 0.0f;
 	public float zRotation = 0.0f;
-	public float yRotation = 0.0f;
-	public float speed = 0.0f;
-//	private float xPosition;
-//	private float zPosition;
 	
 	public float smoothTime = 0.0F;
 	public float xVelocity = 0.0F;
@@ -16,15 +12,11 @@ public class JiggleVision : MonoBehaviour {
 	
 	public WalkerController walkerController;
 	
-	// Use this for initialization
+
 	void Start () {
-//		xPosition = 0;//transform.position.x;
-//		zPosition = 0;//transform.position.z;
-//		walker = gameObject;
 		walkerController = GetComponent<WalkerController>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
 		if(!walkerController.isOnGround) {
